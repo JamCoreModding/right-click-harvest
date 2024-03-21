d32324579cb943c45f06cf9b87eb50f3bfe681fd
James (Jamalam) <james@jamalam.tech>
2024-03-21 17:12:25 +0000

    Revert "made some changes"
    This reverts commit b6648c72cbec47cb36f7ca517680a2c477504652.

--- forge/src/main/java/io/github/jamalam360/rightclickharvest/forge/RightClickHarvestForge.java
+++ forge/src/main/java/io/github/jamalam360/rightclickharvest/forge/RightClickHarvestForge.java
@@ -5,11 +5,10 @@ import io.github.jamalam360.rightclickharvest.RightClickHarvest;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod(RightClickHarvest.MOD_ID)​​
-​​public class RightClickHarvestForge {
-​	pub​lic RightClickHarvestForge() {​
-​		Eve​ntBuses.regis​terModEventBus(RightCli​ckHarv​est.MOD_ID, FMLJa​vaModLo​adingContext.get().getModEv​entBus());
-		Right​ClickHarvest.init();
-	}​​
-}​​
-​​​​
+@Mod(RightClickHarvest.MOD_ID)
+public class RightClickHarvestForge {
+	public RightClickHarvestForge() {
+		EventBuses.registerModEventBus(RightClickHarvest.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
+		RightClickHarvest.init();
+	}
+}
