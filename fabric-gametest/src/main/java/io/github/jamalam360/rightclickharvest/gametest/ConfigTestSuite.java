a2df31138860364f998737a9f2666f2875a9ee1c
Jamalam <james@jamalam.tech>
2025-09-23 12:23:24 +0100

    fix: gametests now pass
    # Conflicts:
    #	fabric-gametest/src/main/java/io/github/jamalam360/rightclickharvest/gametest/ConfigTestSuite.java

--- fabric-gametest/src/main/java/io/github/jamalam360/rightclickharvest/gametest/ConfigTestSuite.java
+++ fabric-gametest/src/main/java/io/github/jamalam360/rightclickharvest/gametest/ConfigTestSuite.java
@@ -1,10 +1,9 @@
 package io.github.jamalam360.rightclickharvest.gametest;
 
-import io.github.jamalam360.rightclickharvest.Config;
 import io.github.jamalam360.rightclickharvest.Config.HungerLevel;
 import io.github.jamalam360.rightclickharvest.RightClickHarvest;
+import net.fabricmc.fabric.api.gametest.v1.GameTest;
 import net.minecraft.core.BlockPos;
-import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestHelper;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -14,9 +13,9 @@ import net.minecraft.world.level.block.CropBlock;
 
 public class ConfigTestSuite {
 
-    private static final BlockPos CROP_CENTRE_POS = new BlockPos(4, 3, 4);
+    private static final BlockPos CROP_CENTRE_POS = new BlockPos(4, 2, 4);
 
-    @GameTest(template = "rightclickharvest-gametest:wheat")
+    @GameTest(structure = "rightclickharvest-gametest:wheat")
     public void testRequireHoeTrueFail(GameTestHelper helper) {
         RightClickHarvest.CONFIG.get().requireHoe = true;
         TestHelper.interact(helper, CROP_CENTRE_POS, ItemStack.EMPTY);
@@ -27,7 +26,7 @@ public class ConfigTestSuite {
         });
     }
 
-    @GameTest(template = "rightclickharvest-gametest:wheat")
+    @GameTest(structure = "rightclickharvest-gametest:wheat")
     public void testRequireHoeTrueSucceed(GameTestHelper helper) {
         RightClickHarvest.CONFIG.get().requireHoe = true;
         TestHelper.interact(helper, CROP_CENTRE_POS, Items.WOODEN_HOE.getDefaultInstance());
@@ -38,7 +37,7 @@ public class ConfigTestSuite {
         });
     }
 
-    @GameTest(template = "rightclickharvest-gametest:wheat")
+    @GameTest(structure = "rightclickharvest-gametest:wheat")
     public void testRequireHoeFalseWithHand(GameTestHelper helper) {
         RightClickHarvest.CONFIG.get().requireHoe = false;
         TestHelper.interact(helper, CROP_CENTRE_POS, ItemStack.EMPTY);
@@ -50,7 +49,7 @@ public class ConfigTestSuite {
         });
     }
 
-    @GameTest(template = "rightclickharvest-gametest:wheat")
+    @GameTest(structure = "rightclickharvest-gametest:wheat")
     public void testRequireHoeFalseWithHoe(GameTestHelper helper) {
         RightClickHarvest.CONFIG.get().requireHoe = false;
         TestHelper.interact(helper, CROP_CENTRE_POS, Items.WOODEN_HOE.getDefaultInstance());
@@ -62,7 +61,7 @@ public class ConfigTestSuite {
         });
     }
 
-    @GameTest(template = "rightclickharvest-gametest:wheat")
+    @GameTest(structure = "rightclickharvest-gametest:wheat")
     public void testHarvestInRadiusFalse(GameTestHelper helper) {
         RightClickHarvest.CONFIG.get().harvestInRadius = false;
         TestHelper.interact(helper, CROP_CENTRE_POS, Items.NETHERITE_HOE.getDefaultInstance());
@@ -74,36 +73,35 @@ public class ConfigTestSuite {
         });
     }
 
-    @GameTest(template = "rightclickharvest-gametest:wheat")
+    @GameTest(structure = "rightclickharvest-gametest:wheat")
     public void testHungerLevelNormal(GameTestHelper helper) {
+	    RightClickHarvest.CONFIG.get().hungerLevel = HungerLevel.NORMAL;
         Player player = helper.makeMockPlayer(GameType.SURVIVAL);
-        float exhaustion = player.getFoodData().getExhaustionLevel();
+        float exhaustion = TestHelper.getPlayerExhaustion(player);
         TestHelper.interact(helper, player, CROP_CENTRE_POS, Items.WOODEN_HOE.getDefaultInstance());
 
         helper.succeedIf(() -> {
             helper.assertBlockProperty(CROP_CENTRE_POS, CropBlock.AGE, 0);
             helper.assertItemEntityPresent(Items.WHEAT);
 
-            if (player.getFoodData().getExhaustionLevel() <= exhaustion) {
+            if (TestHelper.getPlayerExhaustion(player) <= exhaustion) {
                 throw new AssertionError("Player's exhaustion level did not increase");
             }
         });
     }
 
-    @GameTest(template = "rightclickharvest-gametest:wheat")
+    @GameTest(structure = "rightclickharvest-gametest:wheat")
     public void testHungerLevelNone(GameTestHelper helper) {
         RightClickHarvest.CONFIG.get().hungerLevel = HungerLevel.NONE;
         Player player = helper.makeMockPlayer(GameType.SURVIVAL);
-        float exhaustion = player.getFoodData().getExhaustionLevel();
+        float exhaustion = TestHelper.getPlayerExhaustion(player);
         TestHelper.interact(helper, player, CROP_CENTRE_POS, Items.WOODEN_HOE.getDefaultInstance());
 
         helper.succeedIf(() -> {
             helper.assertBlockProperty(CROP_CENTRE_POS, CropBlock.AGE, 0);
             helper.assertItemEntityPresent(Items.WHEAT);
 
-            RightClickHarvest.CONFIG.get().hungerLevel = HungerLevel.NORMAL;
-
-            if (player.getFoodData().getExhaustionLevel() > exhaustion) {
+            if (TestHelper.getPlayerExhaustion(player) > exhaustion) {
                 throw new AssertionError("Player's exhaustion level increased when it shouldn't have");
             }
         });
